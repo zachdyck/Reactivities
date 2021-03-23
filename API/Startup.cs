bc5e79d8e992f06c02953dbbddc4f0c93ade48d1
Zach Dyck <zach_dyck@hotmail.com>
2021-03-22 23:21:19 -0400

    Added MediatR
    Signed-off-by: Zach Dyck <zach_dyck@hotmail.com>

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.OpenApi.Models;
 using Persistence;
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
 
 namespace API
 {
@@ -48,6 +50,8 @@ namespace API
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
                 });
             });
+            
+            services.AddMediatR(typeof(List.Handler).Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
