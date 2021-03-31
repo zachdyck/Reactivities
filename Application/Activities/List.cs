65a1b07169f55136238bce67bf385e4642df8ac9
Zach Dyck <zach_dyck@hotmail.com>
2021-03-31 00:07:49 -0400

    React initial structure
    Signed-off-by: Zach Dyck <zach_dyck@hotmail.com>

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
