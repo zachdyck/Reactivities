4b669faaab8c395c7cea534f08ee8d5b6d1a43f9
Zach Dyck <zach_dyck@hotmail.com>
2021-04-02 00:22:24 -0400

    Added activity dashboard
    Signed-off-by: Zach Dyck <zach_dyck@hotmail.com>

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Grid, GridColumn, List } from 'semantic-ui-react'
+import { Activity } from '../../../app/models/activity'
+import ActivityList from './ActivityList'
+
+interface Props {
+    activities: Activity[];
+}
+
+
+export default function ActivityDashboard({activities}: Props) {
+    return  (
+        <Grid>
+            <Grid.Column width='10'>
+            <ActivityList activities={activities} />
+            </Grid.Column>
+        </Grid>
+    )
+}
